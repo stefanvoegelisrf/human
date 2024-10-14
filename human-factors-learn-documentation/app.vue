5048148891f0442ac4ddd851d29dbd089e18300d
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-10-14 16:26:24 +0200

    add learn documentation web with nuxt and tailwind

---
+++ human-factors-learn-documentation/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <div class="bg-violet">Hi</div>
+  </div>
+</template>
---
+++ human-factors-learn-documentation/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      "violet": "#5c2751ff",
+      "cerulean": "#2d728fff",
+      "pale-purple": "#e3d0d8ff",
+      "tomato": "#ef6351ff",
+      "emerald": "#61d095ff"
+    }
+  },
+  plugins: [],
+}
+
